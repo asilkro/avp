7328515bed045957d03dc64983ac996e9d8ec71f
asilkro <asilkro@me.com>
2021-06-23 17:59:52 -0700

    WIP: More tests

--- src/avp/location.rs
+++ src/avp/location.rs
@@ -1,6 +1,7 @@
 use crate::{avp::climate::Climate, Result};
 use std::path::Path;
 use std::fs::File;
+use std::fs::Metadata;
 use serde::{Serialize, Deserialize};
 use serde_yaml;
 
@@ -21,7 +22,19 @@ pub struct Locations {
 impl Locations {
     pub fn new(path: &Path) -> Result<Self> {
         let reader = File::open(path)?;
-        let locs = serde_yaml::from_reader(reader)?;
+        // let locs = serde_yaml::from_reader(reader)?;
+        let locs = match reader.metadata()?.len() {
+            0 => Self {locations: Vec::new()}, // if 0 do this
+            _ => serde_yaml::from_reader(reader)? // else, do that
+        };
         Ok(locs)
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    pub fn len(&self) -> usize {
+        self.locations.len()
     }
 }
--- src/avp/unit_tests.rs
+++ src/avp/unit_tests.rs
@@ -5,42 +5,38 @@ use super::*;
 use std::io::{Error as IoError, ErrorKind};
 use std::fs;
 
-/*
-#[test]
-fn two_plus_two() {
-    // Given (minimal setup for test)
-    let expected = 4;
-    let a = 2;
-    let b = 2;
-
-    // When (execution of test, capture result)
-    let result = a+b;
-
-    // Then (check the actual matches what is expected)
-    assert_eq!(result, expected);
-}
-*/
-
 #[test]
 fn new__non_existent_file_causes_error() {
     // Given
     let non_existent_file = "fakefile.yml".as_ref();
 
     // When
-    let actual = Location::new(non_existent_file);
+    let actual = Locations::new(non_existent_file);
 
     // Then
     assert!(actual.is_err());
 }
 
 #[test]
-fn new__existing_file_succeeds() {
+fn new__empty_existing_file_succeeds() {
     // Given
     let temp_file = tempfile::NamedTempFile::new().unwrap();
 
     // When -
-    let result = Location::new(temp_file.path());
+    let result = Locations::new(temp_file.path());
 
     // Then
-    assert!(result.is_ok());
+    // Unwrap for test only, plz no ship
+    assert!(result.is_ok(), "{:?}", result);
+    assert!(result.unwrap().is_empty());
+
 }
+
+// #[test]
+// fn new__invalid_file_returns_error() {
+//     // Given - empty file
+//     let invalid_file =
+//     // When - an empty file is read
+//
+//     // Then -
+// }
